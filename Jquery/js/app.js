f5f155e82eff416a5c104c6dc695bc9102a30782
khatrisaugat <khatri.saugat55@gmail.com>
2020-12-04 10:43:46 +0545

    password visibility

--- Jquery/js/app.js
+++ Jquery/js/app.js
@@ -12,4 +12,17 @@ jQuery(function () {
         width -= 5;
         $('.imageClass').css('width', width + '%');
     });
+
+    $('.eyecon').on('click', function () {
+        if ($('#password').attr('type') === 'password') {
+            $('#password').attr('type', 'text');
+            this.className = "eyecon fas fa-eye-slash";
+            // $('.eyecon').addClass('fa-eye-slash');
+
+        } else {
+            $('#password').attr('type', 'password');
+            this.className = "eyecon fas fa-eye";
+        }
+
+    })
 });
